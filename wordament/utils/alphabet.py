1f02b86542a5921b068eb8b52ae3a0771af0f80d
Sandeep Dasika <dasika.sandy@gmail.com>
2014-01-13 23:37:36 +0530

    added new image set that contain points and edited points accordingly

--- wordament/utils/alphabet.py
+++ wordament/utils/alphabet.py
@@ -1,33 +1,35 @@
 from PyQt4 import QtGui, QtCore
 import random
 from random import randint
+
+
 _points = {'': 0,
           'a':1,
-          'b':2,
+          'b':3,
           'c':3,
-          'd':3,
+          'd':2,
           'e':1,
           'f':4,
-          'g':4,
+          'g':2,
           'h':4,
-          'i':2,
-          'j':4,
-          'k':3,
-          'l':3,
-          'm':4,
-          'n':2,
-          'o':2,
-          'p':2,
-          'q':2,
-          'r':2,
-          's':2,
-          't':2,
-          'u':4,
-          'v':5,
-          'w':5,
-          'x':6,
-          'y':5,
-          'z':6,
+          'i':1,
+          'j':8,
+          'k':5,
+          'l':1,
+          'm':3,
+          'n':1,
+          'o':1,
+          'p':3,
+          'q':10,
+          'r':1,
+          's':1,
+          't':1,
+          'u':1,
+          'v':4,
+          'w':4,
+          'x':8,
+          'y':4,
+          'z':10,
           'in':6,
           'es':12,
           'pr':6, }
@@ -41,6 +43,5 @@ class Alphabet(object):
     def __init__(self, letter):
         self.letter = letter
         '''image path wrt to index.py'''
-        self.image = 'utils/images/alphabet/' + self.letter + '.png'
+        self.image = 'utils/images/alphabet'  + '/' + self.letter + '.png'
         self.points = _points[self.letter]
-        self.weight = -1
