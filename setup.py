12eebaba8c7f5ebfdc0f402f23d4298947f8ece4
Sandeep Dasika <dasika.sandy@gmail.com>
2014-01-20 12:54:32 +0530

    removed unwanted files. ready for release.

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-      name = 'PracticeWordament',
-      version = '0.1',
-      author = 'Sandeep Dasika',
-      author_email = 'dasika.sandy@gmail.com',
-      
-      )
